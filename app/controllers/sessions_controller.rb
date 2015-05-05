a66e0d610db5ac6cb3dd941c602230cbf7265c1a
Lara Finnegan <lcfinnegan@gmail.com>
2015-05-04 21:16:43 -0400

    completed login functionality

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,5 +4,14 @@ class SessionsController < ApplicationController
   end
 
   def create
+    user = User.find_by(name: params[:session][:name])
+    if user && user.authenticate(params[:session][:password])
+      log_in user
+      redirect_to root_path
+    else
+      flash.now[:danger] = "invalid name/password"
+      render :new
+    end
   end
+
 end
