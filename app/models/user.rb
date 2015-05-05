93569d184f036c667a194929a45a2ad487db3b8b
Lara Finnegan <lcfinnegan@gmail.com>
2015-05-04 20:00:32 -0400

    installed bcrypt and added a password digest column to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ActiveRecord::Base
   has_many :posts
 
   validates :name, presence: true
+
+  has_secure_password
+  
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150503192714) do
+ActiveRecord::Schema.define(version: 20150504235841) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -54,8 +54,9 @@ ActiveRecord::Schema.define(version: 20150503192714) do
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.string   "password_digest"
   end
 
 end
