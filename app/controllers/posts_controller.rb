337f14fdf29968d8e1a8305e846d25b814a7b688
Lara Finnegan <lcfinnegan@gmail.com>
2015-05-05 18:54:13 -0400

    rolled back to get rid of ckeditor and paperclip; added edit/update post form and edit links again

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,5 @@
 class PostsController < ApplicationController
+  before_action :logged_in_user, only: [:new, :create, :edit, :update, :destroy]
 
   def index
     @tags = Tag.all
@@ -23,6 +24,18 @@ class PostsController < ApplicationController
     end
   end
 
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    if @post.update_attributes(post_params)
+      redirect_to @post
+    else
+      render :edit
+    end
+  end
 
   def show
     @tags = Tag.all
@@ -33,6 +46,13 @@ class PostsController < ApplicationController
 
   def post_params
     params.require(:post).permit(:title, :content, :tags)
+  end
+
+  def logged_in_user
+    unless logged_in?
+      flash[:danger] = "Only admins may create and edit posts"
+      redirect_to root_url
+    end
   end
 
 end
