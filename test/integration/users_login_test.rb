8e19d7367eab42413ef955f0e50301d85b50f689
Lara Finnegan <lcfinnegan@gmail.com>
2015-05-04 21:43:58 -0400

    add more login integration tests

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -6,6 +6,16 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     @user = users(:lara)
   end
 
+  test "login with invalid info" do 
+    get login_path
+    assert_template 'sessions/new'
+    post login_path, session: { name: " ", password: " " }
+    assert_template 'sessions/new'
+    assert_not flash.empty?
+    get root_path
+    assert flash.empty?
+  end
+
   test "login with valid information" do 
     get posts_path
     assert_select "a[href=?]", new_post_path, count: 0
