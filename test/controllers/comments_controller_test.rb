f4e4c54338a1c24fad84bfe9a5b150125fc4bb2f
Lara Finnegan <lcfinnegan@gmail.com>
2015-05-09 13:47:07 -0400

    added some tests and a little refactoring

--- test/controllers/comments_controller_test.rb
+++ test/controllers/comments_controller_test.rb
@@ -1,7 +1,13 @@
 require 'test_helper'
 
 class CommentsControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @comment = comments(:comment_one)
+  end
+
+  test "should redirect destroy when not logged in" do 
+    assert_no_difference 'Comment.count' do 
+      delete :destroy, id: @comment.id
+    end
+  end
 end
